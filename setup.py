6164059332a857cd17d97d81f9641ee5548d6d2c
Vasudha Jha <reachvasudha27@gmail.com>
2023-07-26 14:13:42 -0700

    add workflows for release and publish to pypi + bump version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="genomap",
-    version="0.1.0",
+    version="1.0.0",
     author="Md Tauhidul Islam",
     author_email="tauhid@stanford.edu",
     description="Leveraging cell-cell similarity from gene expression data for high-performance spatial and temporal cellular mappings.",
